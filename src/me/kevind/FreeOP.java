4d30ebd3fd4cc61a05c5ec8b88b3a64de2867576
Kevin D <kevin.diangelis@gmail.com>
2022-12-31 01:03:14 -0500

    i did it properly

--- src/me/kevind/FreeOP.java
+++ src/me/kevind/FreeOP.java
@@ -6,6 +6,8 @@ import me.kevind.listeners.ChatListener;
 import me.kevind.listeners.JoinListener;
 import me.kevind.listeners.QuitListener;
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FreeOP extends JavaPlugin {
@@ -23,6 +25,11 @@ public final class FreeOP extends JavaPlugin {
         }else {
             getLogger().warning("Please install PlaceholderAPI!");
             Bukkit.getPluginManager().disablePlugin(this);
+        }
+        getLogger().info("Setting gamerules...");
+        for (World world : Bukkit.getWorlds()) {
+            world.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, false);
+            world.setGameRule(GameRule.COMMAND_BLOCK_OUTPUT, false);
         }
         getLogger().info("Registering commands...");
         getCommand("gmc").setExecutor(new GmcCommand());
